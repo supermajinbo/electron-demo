f2b191e56d55acfdd99433588040cdf199961887
jinboma <jinboma@tencent.com>
2021-10-27 20:18:11 +0800

    electron demo

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+// 在 Electron 中，只有在 app 模块的 ready 事件被激发后才能创建浏览器窗口
+app.whenReady().then(() => {
+  createWindow();
+
+  // 监控窗口激活事件
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
